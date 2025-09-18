26dc1839e0a87c240e323d69c0b30baefb33b96d
TrippJosh <142738002+TrippJosh@users.noreply.github.com>
2025-09-18 10:28:18 +0000

    Fixed pygame items, will release version for debugging (cannot create local environment on school computer)

--- main.py
+++ main.py
@@ -4,18 +4,40 @@ import sys
 import pygame
 
 pygame.init()
-running = True
 
-res = (720,720)
-screen = pygame.display.set_mode(res)
+res = (720, 720)
 white = (210, 210, 210)
 grey = (170, 170, 170)
 darkGrey = (100, 100, 100)
 
+screen = pygame.display.set_mode(res)
+smallfont = pygame.font.SysFont('Corbel', 35)
+text = smallfont.render('quit', True, white)
 width = screen.get_width()
 height = screen.get_height()
-smallfont = pygame.font.SysFont('Corbel',35)
-text = smallfont.render('quit' ,True, white)
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            if event.button == 1:
+                if 10 <= event.pos[0] <= 100 and 10 <= event.pos[1] <= 50:
+                    pygame.quit()
+                    sys.exit()
+
+    screen.fill(grey)
+    mouse = pygame.mouse.get_pos()
+
+    if width/2 <= mouse[0] <= width/2+140 and height/2 <= mouse[1] <= height/2+40:
+        pygame.draw.rect(screen,white,[width/2,height/2,140,40])
+        
+    else:
+        pygame.draw.rect(screen,darkGrey,[width/2,height/2,140,40])
+
+    screen.blit(text , (width/2+50,height/2))
+    pygame.display.update() 
 
 def genAlgorithm():
     """generates numbers for the amount of nearby mines"""
@@ -47,16 +69,3 @@ def genAlgorithm():
     
     finally:
         print("World generation complete")
-
-##important stuff here not usable until integrated
-running = True
-while running:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-
-    screen.fill(grey)  # Fill background
-    screen.blit(text, (10, 10))  # Draw the "quit" text
-    pygame.display.flip()  # Update the display
-
-pygame.quit()
