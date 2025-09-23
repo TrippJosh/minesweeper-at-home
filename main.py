2e964538b5b2fb627da29d5518c3c464dafa085f
TrippJosh <142738002+TrippJosh@users.noreply.github.com>
2025-09-23 12:21:23 +0000

    Removed Pygame items, will continue text-based development

--- main.py
+++ main.py
@@ -1,43 +1,5 @@
 import random
 import time
-import sys
-import pygame
-
-pygame.init()
-
-res = (720, 720)
-white = (210, 210, 210)
-grey = (170, 170, 170)
-darkGrey = (100, 100, 100)
-
-screen = pygame.display.set_mode(res)
-smallfont = pygame.font.SysFont('Corbel', 35)
-text = smallfont.render('quit', True, white)
-width = screen.get_width()
-height = screen.get_height()
-
-while True:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-
-        if event.type == pygame.MOUSEBUTTONDOWN:
-            if event.button == 1:
-                if 10 <= event.pos[0] <= 100 and 10 <= event.pos[1] <= 50:
-                    pygame.quit()
-                    sys.exit()
-
-    screen.fill(grey)
-    mouse = pygame.mouse.get_pos()
-
-    if width/2 <= mouse[0] <= width/2+140 and height/2 <= mouse[1] <= height/2+40:
-        pygame.draw.rect(screen,white,[width/2,height/2,140,40])
-        
-    else:
-        pygame.draw.rect(screen,darkGrey,[width/2,height/2,140,40])
-
-    screen.blit(text , (width/2+50,height/2))
-    pygame.display.update() 
 
 def genAlgorithm():
     """generates numbers for the amount of nearby mines"""
