dc2bf9731e4a8ec5981a44718188ae830ea22915
TrippJosh <142738002+TrippJosh@users.noreply.github.com>
2025-09-30 13:13:47 +0000

    Fixed loc issue and made game actually worthy of being used.

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import random
 import time
+import sys
 
 def printS(text):
     print(text)
@@ -9,7 +10,7 @@ def genAlgorithm():
     """generates, randomly, whether or not a mine is at a space"""
     try:
         temp = random.randint(1, 100)
-        if temp < 61:
+        if temp < 34:
             mineNum = 1
         else:
             mineNum = 0
@@ -22,58 +23,63 @@ def genAlgorithm():
     
 def flag(row, col):
     """handles flagging"""
-    print(f"Flagging at {row}, {col}")
-    revealed[row][col] = 'F'
+    try:
+        print(f"Flagging at {col+1}, {row+1}")
+        revealed[row][col] = 'F'
+    except:
+        print("Error processing flag command.")
 
-def click (row, col):
-    """handles clicking and searches surrounding mines (3x3 grid)"""
-    print(f"Clicking at {row}, {col}")
-    if grid[row][col] == 1:
-        print("You hit a mine! Game over.")
-        global playing
-        playing = False
-    else:
-        ticking = 8
-        displayNum = 0
-        tempRow = row
-        tempCol = col
-        while ticking != 0:
-            if tempRow > 0 and tempCol > 0 and grid[tempRow - 1][tempCol - 1] == 1:
-                displayNum += 1
-            else:
-                revealed[tempRow - 1][tempCol - 1] = 0
-            if tempRow > 0 and grid[tempRow - 1][tempCol] == 1:
-                displayNum += 1
-            else:
-                revealed[tempRow - 1][tempCol] = 0
-            if tempRow > 0 and tempCol < size - 1 and grid[tempRow - 1][tempCol + 1] == 1:
-                displayNum += 1
-            else:
-                revealed[tempRow - 1][tempCol + 1] = 0
-            if tempCol > 0 and grid[tempRow][tempCol - 1] == 1:
-                displayNum += 1
-            else:
-                revealed[tempRow][tempCol - 1] = 0
-            if tempCol < size - 1 and grid[tempRow][tempCol + 1] == 1:
-                displayNum += 1
-            else:
-                revealed[tempRow][tempCol + 1] = 0
-            if tempRow < size - 1 and tempCol > 0 and grid[tempRow + 1][tempCol - 1] == 1:
-                displayNum += 1
-            else:
-                revealed[tempRow + 1][tempCol - 1] = 0
-            if tempRow < size - 1 and grid[tempRow + 1][tempCol] == 1:
-                displayNum += 1
-            else:
-                revealed[tempRow + 1][tempCol] = 0
-            if tempRow < size - 1 and tempCol < size - 1 and grid[tempRow + 1][tempCol + 1] == 1:
-                displayNum += 1
-            else:
-                revealed[tempRow + 1][tempCol + 1] = 0
-            ticking -= 1
+first_click = True
 
-        if ticking == 0:
+def click(row, col):
+    """handles clicking process"""
+    try:
+        global first_click, playing
+        print(f"Clicking at {col+1}, {row+1}")
+        if revealed[row][col] == 'F':
+            print(f"Tile at {row+1}, {col+1} is flagged. Cannot click a flagged tile.")
+            return
+        if grid[row][col] == 1:
+            print("You hit a mine! Game over.")
+            question = input("Would you like to play again? (y/n): ").lower()
+            if question == 'y':
+                import os
+                os.execv(sys.executable, ['python'] + sys.argv)
+            else:
+                playing = False
+        else:
+            # Count nearby mines
+            displayNum = 0
+            for dr in [-1, 0, 1]:
+                for dc in [-1, 0, 1]:
+                    if dr == 0 and dc == 0:
+                        continue
+                    nr, nc = row + dr, col + dc
+                    if 0 <= nr < size and 0 <= nc < size:
+                        if grid[nr][nc] == 1:
+                            displayNum += 1
+
             revealed[row][col] = displayNum
+
+            if first_click:
+                for dr in [-1, 0, 1]:
+                    for dc in [-1, 0, 1]:
+                        nr, nc = row + dr, col + dc
+                        if 0 <= nr < size and 0 <= nc < size:
+                            if grid[nr][nc] == 0 and revealed[nr][nc] == ".":
+                                neighbor_mines = 0
+                                for dr2 in [-1, 0, 1]:
+                                    for dc2 in [-1, 0, 1]:
+                                        if dr2 == 0 and dc2 == 0:
+                                            continue
+                                        nnr, nnc = nr + dr2, nc + dc2
+                                        if 0 <= nnr < size and 0 <= nnc < size:
+                                            if grid[nnr][nnc] == 1:
+                                                neighbor_mines += 1
+                                revealed[nr][nc] = neighbor_mines
+                first_click = False
+    except:
+        print("Error processing click command.")
 
 #startup/intro sequence
 printS("Josh's Minesweeper")
@@ -116,25 +122,48 @@ while mineTotal != 0: # fills the revealed grid with .
         columnCounter = 0
         rowCounter += 1
 
+startRow = 1
+startCol = 1
+foundEmpty = False
+while foundEmpty == False: # finds an empty space to start the game
+    try:
+        if grid[startRow][startCol] == 0:
+            click(startRow, startCol)
+            foundEmpty = True
+        else:
+            startRow += 1
+            startCol += 1
+    except:
+        print("Error finding starting position.")
+
 if debug == True:
-    print("DEBUG MODE ENABLED")
-    print("Mine grid:")
-    for row in grid:
-        print(' '.join(str(cell) for cell in row))
+    try:
+        print("DEBUG MODE ENABLED")
+        print("Mine grid:")
+        for row in grid:
+            print(' '.join(str(cell) for cell in row))
+    except:
+        print("Error displaying debug information.")
+
+##legend
+print("Legend: F = Flagged, . = Unrevealed, Numbers = ")
 
 while playing == True:
-    print("Current Board:")
-    for row in revealed:
-        print(' '.join(str(cell) for cell in row))
+    try:
+        print("Current Board:")
 
-    userInput = input("-> ")
-    cmd, arg = userInput.split()
-    col = ord(arg[0].lower()) - ord('a')
-    row = int(arg[1:]) - 1
+        for row in revealed:
+            print(' '.join(str(cell) for cell in row))
 
-    if cmd == "flag":
-        flag(row, col)
-    elif cmd == "click":
-        click(row, col)
-    else:
+        userInput = input("-> ")
+        cmd, arg = userInput.split()
+        row = int(arg[1]) - 1
+        col = int(arg[0]) - 1
+
+        if cmd == "flag":
+            flag(row, col)
+        elif cmd == "click":
+            click(row, col)
+    except:
         print("Unknown command")
+    
